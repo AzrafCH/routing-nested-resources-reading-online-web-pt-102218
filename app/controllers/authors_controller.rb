7ad79dae803d434f4764ef14838a0c92091a6711
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-19 22:42:37 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,18 +4,18 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def posts_index 
+  def posts_index
     @author = Author.find(params[:id])
     @posts = @author.posts
     render template: 'posts/index'
-  end 
+  end
 
-  def post 
+  def post
     @author = Author.find(params[:id])
 
     @post = Post.find(params[:id])
     render template: 'posts/show'
-  end 
-  
+  end
+
 
 end
